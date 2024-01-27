ba4dcb93c53594a065476cf0d4794f9b7367731d
Dolphcode <bastikulit@gmail.com>
2024-01-27 15:40:52 -0500

    Added more functionality to background

--- Scripts/Environment/stage_platform.gd
+++ Scripts/Environment/stage_platform.gd
@@ -2,14 +2,14 @@
 class_name StagePlatform
 extends Node3D
 
+enum ScaleMode { HEIGHT, WIDTH }
+
 @export var dimensions: Vector2i = Vector2i(0, 0)
 @export var plat_scale: int = 1
-@export var world_size: float = 2.0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	generate_platforms()
 	pass # Replace with function body.
 
 
@@ -18,11 +18,14 @@ func _process(delta):
 	pass
 
 func generate_platforms():
+	# Get rid of all exising children to regenerate tiles, this is the easiest approach
+	for child in get_children():
+		child.queue_free()
+	
 	var tile = load("res://Scenes/Environment/grid_tile.tscn")
 	for x in range(0, dimensions.x * plat_scale):
 		for y in range(0, dimensions.y * plat_scale):
 			var newtile = tile.instantiate()
-			print(newtile)
 			newtile.position.x = x
 			newtile.position.y = y
 			add_child(newtile)
