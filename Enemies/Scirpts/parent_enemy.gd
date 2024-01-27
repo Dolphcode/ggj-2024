ea121e27dd87ec45bb783c4e5220d86ddb6fd479
Miguel Aldana <miguelaldana2003@gmail.com>
2024-01-27 15:07:04 -0500

    Added parent enemy

--- Enemies/Scirpts/parent_enemy.gd
+++ Enemies/Scirpts/parent_enemy.gd
@@ -1,12 +1,22 @@
 extends CharacterBody3D
 
+const SPEED = 2.0
+const JUMP_VELOCITY = 4.5
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 5.0
+# direction
+var input_dir = Vector3(1,0,0)
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -14,12 +24,13 @@ func _physics_process(delta):
 		velocity.y -= gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	#	velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	# Direction
+	input_dir = Vector3(1,0,0)
+	
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -27,3 +38,8 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+
+
+func _on_detection_area_body_entered(body):
+	pass
