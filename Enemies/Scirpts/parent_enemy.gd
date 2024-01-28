f2ba847f35454253eca151af492641babc7189ec
migue <migue@DESKTOP-SDTJSU2>
2024-01-27 21:06:58 -0500

    Add jump to player

--- Enemies/Scirpts/parent_enemy.gd
+++ Enemies/Scirpts/parent_enemy.gd
@@ -56,7 +56,5 @@ func _physics_process(delta):
 	move_and_slide()
 	
 
-
-
 func _on_wall_detection_area_entered(area):
-	print("Wall!")
+	scale.x = scale.x * -1
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,18 +1,20 @@
 extends CharacterBody3D
 
-
-const SPEED = 5.0
-const JUMP_VELOCITY = 5.0
+@export var health = 3
+@export var SPEED = 5.0
+@export var JUMP_VELOCITY = 9.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-
 func _physics_process(delta):
+	
+	position.z = 0
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
