b00874a81800d96a608b15bc1687c24b90645bf7
Dolphcode <bastikulit@gmail.com>
2024-01-30 16:59:49 -0500

    Created level layout creator

--- Scripts/Editor/generate_layout.gd
+++ Scripts/Editor/generate_layout.gd
@@ -1,11 +1,37 @@
-extends Node
+@tool
+class_name GenerateLayout
+extends EditorScript
 
+const LayoutMapClass = preload("res://Scripts/Editor/layout_tilemap.gd")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+# Called when the script is executed (using File -> Run in Script Editor).
+func _run():
+	var tilemap: LayoutTileMap = get_tilemap(get_scene())
+	
+	var output = ""
+	for y in range(0, tilemap.dims.y):
+		for x in range(0, tilemap.dims.x):
+			output += tilemap.get_cell_tile_data(0, Vector2i(x, y)).get_custom_data("Tile Data")
+		output += "-\n" if y < tilemap.dims.y - 1 else ""
+	
+	var filepath = tilemap.export_path + tilemap.export_filename + ".txt"
+	var i = 0
+	while (FileAccess.file_exists(filepath)):
+		i += 1
+		filepath = tilemap.export_path + tilemap.export_filename + "(" + str(i) + ").txt"
+	var file = FileAccess.open(filepath, FileAccess.WRITE)
+	file.store_string(output)
+	file.close()
+			
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+# We're assuming there's only one TileMap
+func get_tilemap(scene):
+	if (scene is LayoutMapClass):
+		return scene
+		
+	var tilemap
+	for child in scene.get_children():
+		tilemap = get_tilemap(child)
+		if tilemap != null:
+			return tilemap
+	
---
+++ Scripts/Editor/layout_tilemap.gd
@@ -0,0 +1,7 @@
+class_name LayoutTileMap
+extends TileMap
+
+@export var export_path: String
+@export var export_filename: String = "Unnamed"
+@export var dims: Vector2i
+
